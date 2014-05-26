4890d7e082d471e546f71689ec801321a062a76f
Ferdi van der Werf <efcm@vdwerf.eu>
2014-05-26 16:01:35 +0200

    Add listen mode to template for pool

--- definitions/php_fpm_pool.rb
+++ definitions/php_fpm_pool.rb
@@ -36,6 +36,7 @@ define :php_fpm_pool, :template => "pool.conf.erb", :enable => true do
       variables(
         :pool_name => pool_name,
         :listen => params[:listen],
+        :listen_mode => params[:listen_mode],
         :allowed_clients => params[:allowed_clients],
         :user => params[:user],
         :group => params[:group],
